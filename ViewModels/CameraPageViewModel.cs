fd7625610e8be4e961ac8e1e7751629c02356e18
Иванов Александр <sana-ivanov1@yandex.ru>
2023-08-29 21:29:19 +0500

    Update

--- ViewModels/CameraPageViewModel.cs
+++ ViewModels/CameraPageViewModel.cs
@@ -1,21 +1,22 @@
 ﻿using DevExpress.Mvvm;
 using System;
 using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
+using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using DotNetProjects.MjpegProcessor;
+
+
+
 
 namespace Macroscope.ViewModels;
 
 public class CameraPageViewModel : ViewModelBase
 {
+    private BitmapImage _videoImage;
 
-    private BitmapFrame _videoImage;
-
-    public BitmapFrame VideoImage
+    public BitmapImage VideoImage
     {
         get { return _videoImage; }
         set
@@ -27,35 +28,48 @@ public class CameraPageViewModel : ViewModelBase
 
     public ICommand ShowVideo
     {
-        get
-        {
-            return new DelegateCommand(() =>
-            {
-                UpdateVideoStream();
-            });
-        }
+        get { return new DelegateCommand(() => { UpdateVideoStream(); }); }
     }
-
 
     private async void UpdateVideoStream()
     {
         try
         {
-            using (HttpClient client = new HttpClient())
-            {
-                using (var response = await client.GetAsync("http://demo.macroscop.com:8080/mobile?login=root&channelid=2016897c-8be5-4a80-b1a3-7f79a9ec729c&resolutionX=640&resolutionY=480&fps=25"))
-                {
-                    using (var stream = await response.Content.ReadAsStreamAsync())
-                    {
-                        // Разбираем поток и выделяем кадры
-                        var decoder = new JpegBitmapDecoder(stream, BitmapCreateOptions.None, BitmapCacheOption.Default);
 
-                        foreach (var frame in decoder.Frames)
-                        {
-                            // Отображение текущего кадра в элементе VideoImage
-                            VideoImage = frame;
-                        }
+            // Создаем объект-запрос
+            WebRequest request =
+                WebRequest.Create(
+                    "http://demo.macroscop.com:8080/mobile?login=root&channelid=2016897c-8be5-4a80-b1a3-7f79a9ec729c&resolutionX=640&resolutionY=480&fps=25");
+
+            // Получаем ответ от сервера
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        // Создаем объект
+                        MemoryStream memoryStream = new MemoryStream(buffer, 0, bytesRead);
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.StreamSource = memoryStream;
+                        bitmapImage.EndInit();
+
+                        // // Устанавливаем изображение в элемент Image
+                        VideoImage = bitmapImage;
                     }
+
+                    // Через временный файл
+                    /*// Создаем временный файл для сохранения видео
+                    string tempFilePath = Path.GetTempFileName();
+
+                    // Сохраняем видео-поток во временном файле
+                    using (FileStream fileStream = File.Create(tempFilePath))
+                    {
+                        await stream.CopyToAsync(fileStream);
+                    }*/
                 }
             }
         }
@@ -64,5 +78,6 @@ public class CameraPageViewModel : ViewModelBase
             MessageBox.Show($"Error occurred: {ex.Message}");
         }
     }
-
 }
+
+
